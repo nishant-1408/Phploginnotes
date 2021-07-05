69ca2825ba58a5b32f00e3b6474df1e5f6bcc228
nishant-1408 <74047349+nishant-1408@users.noreply.github.com>
2021-07-05 12:34:15 +0530

    Add files via upload

---
+++ _nav.php
@@ -0,0 +1,42 @@
+<?php 
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+  $loggedin= true;
+}
+else{
+  $loggedin = false;
+}
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="/LOGIN">LOGIN USER</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/LOGIN/welcome.php">Home <span class="sr-only">(current)</span></a>
+      </li>';
+
+      if(!$loggedin){
+      echo '<li class="nav-item">
+        <a class="nav-link" href="/LOGIN/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/LOGIN/signup.php">Signup</a>
+      </li>';
+      }
+      if($loggedin){  
+      echo '<li class="nav-item">
+        <a class="nav-link" href="/LOGIN/logout.php">Logout</a>
+      </li>';
+      echo '<li class="nav-item">
+        <a class="nav-link" href="/LOGIN/Notes.php">Notes</a>
+      </li>';
+    }
+       
+      
+    echo '</ul>
+    
+  </div>
+</nav>';
+?>
---
+++ modal1.php
@@ -0,0 +1,31 @@
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editModalLabel">Edit this Note</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span class="cross" aria-hidden="true">×</span>
+          </button>
+        </div>
+        <form action="/LOGIN/Notes.php" method="POST">
+          <div class="modal-body">
+            <input type="hidden" name="snoEdit" id="snoEdit">
+            <div class="form-group">
+              <label for="title">Note Title</label>
+              <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group">
+              <label for="desc">Note Description</label>
+              <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+            </div> 
+          </div>
+          <div class="modal-footer d-block mr-auto">
+            <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-light">Save changes</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
